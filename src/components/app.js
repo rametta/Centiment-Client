ccdca0e8ebac0cf781df2a679b92c074d4493cc1
Jason <rametta@outlook.com>
2016-11-12 00:23:34 -0500

    pages added

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Link } from 'react-router';
 
 // Material UI Components
 import AppBar from 'material-ui/AppBar';
@@ -14,24 +15,40 @@ export default class App extends Component {
     super();
     injectTapEventPlugin();
     this.state = {
-      isDrawerVisible: false
+      isDrawerVisible: false,
+      menuItems: [
+        {path: '/', title: 'Home'},
+        {path: '/quiz', title: 'Quiz'}
+      ]
     }
+  }
+
+  toggleDrawer() {
+    const { isDrawerVisible } = this.state;
+    this.setState({ isDrawerVisible: !isDrawerVisible });
+  }
+
+  renderMenuItems() {
+    return this.state.menuItems.map((item, i) => {
+      return <Link to={item.path} key={i} onTouchTap={() => this.toggleDrawer()}><MenuItem>{item.title}</MenuItem></Link>
+    });
   }
 
   render() {
     const { isDrawerVisible } = this.state;
-    
+
     return (
       <MuiThemeProvider>
         <div>
           <AppBar
             title="YHacks 2016 - Client"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
-            onTouchTap={() => this.setState({ isDrawerVisible: !isDrawerVisible })}
+            onTouchTap={() => this.toggleDrawer()}
           />
           <Drawer open={isDrawerVisible}>
-            <MenuItem>Menu Item</MenuItem>
+            {this.renderMenuItems()}
           </Drawer>
+          {this.props.children}
         </div>
       </MuiThemeProvider>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
+
+// Custom Components
 import App from './components/app';
+import Home from './components/home';
+import Quiz from './components/quiz';
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/" component={App} />
+    <Route component={App}>
+      <Route path="/" component={Home}></Route>
+      <Route path="/quiz" component={Quiz}></Route>
+    </Route>
   </Router>
   , document.getElementById('root'));
