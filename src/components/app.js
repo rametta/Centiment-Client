70a7c4cabc51c1a776a3218209c27fa00920a732
Jason <rametta@outlook.com>
2016-11-12 00:04:48 -0500

    removed redux

--- src/components/app.js
+++ src/components/app.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+// Material UI Components
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
 export default class App extends Component {
 
   constructor() {
     super();
     injectTapEventPlugin();
+    this.state = {
+      isDrawerVisible: false
+    }
   }
 
   render() {
+    const { isDrawerVisible } = this.state;
+    
     return (
       <MuiThemeProvider>
         <div>
-          <p>yhacks16</p>
+          <AppBar
+            title="YHacks 2016 - Client"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onTouchTap={() => this.setState({ isDrawerVisible: !isDrawerVisible })}
+          />
+          <Drawer open={isDrawerVisible}>
+            <MenuItem>Menu Item</MenuItem>
+          </Drawer>
         </div>
       </MuiThemeProvider>
     );
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, Link, browserHistory } from 'react-router';
-
 import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App} />
-    </Router>
-  </Provider>
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>
   , document.getElementById('root'));
