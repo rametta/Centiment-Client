d5e3a4f42ca7c436230e6a27042cca9c15360785
Jason <rametta@outlook.com>
2016-11-11 21:35:00 -0500

    setup done

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    injectTapEventPlugin();
+  }
+
   render() {
     return (
-      <div>yhacks16</div>
+      <MuiThemeProvider>
+        <div>
+          <p>yhacks16</p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
