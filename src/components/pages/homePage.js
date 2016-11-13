4ce64f12630df743b0c03ac3c32d18304fd552c3
helfi92 <helfi92@gmail.com>
2016-11-13 02:11:18 -0500

    style home page

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -2,36 +2,30 @@ import React, { Component } from 'react';
 import DocumentTitle from 'react-document-title';
 import Chart from '../chart';
 import Logo from '../logo';
+import { Jumbotron } from 'react-bootstrap';
 
 export default class HomePage extends Component {
 	render() {
 		return (
 			<DocumentTitle title="Home">
-				<div className="container">
-					<div className="row text-center">
+				<div>
+					<Jumbotron style={{'paddingLeft': '30px', 'paddingRight':'30px'}}>
 						<h1>Do you want to make cents?</h1>
-					</div> 
-					<div className="row">
-						<div className="col-md-4">
-							<img className="img-responsive" src="https://coronadotimes.com/wp-content/uploads/2016/06/ascending-graph.png" height="250" />
-							<p>Performance metrics tracking on  top news 
-and social media content</p>
+						<div style={{marginTop: '3em'}}className="row">
+							<div className="col-md-4">
+								<img className="img-responsive jumbotron-image" src="https://coronadotimes.com/wp-content/uploads/2016/06/ascending-graph.png"  />
+								<p>Performance metrics tracking on  top news and social media content</p>
+							</div>
+							<div className="col-md-4">
+								<img className="img-responsive jumbotron-image"  src="https://www.deepcoredata.com/wp-content/uploads/2016/06/small_1420.png"  />
+								<p>Sentiment Analysis of data using our robust machine learning algorithm</p>
+							</div>
+							<div className="col-md-4">
+								<img className="img-responsive jumbotron-image" src="http://bav0.com/wp-content/uploads/sites/11/2014/06/pie-chart.png" />
+								<p>Portfolio optimization and financial literacy to help any beginner investor make smarter choices</p>
+							</div>
 						</div>
-						<div className="col-md-4">
-							<img className="img-responsive" height="150" src="https://www.deepcoredata.com/wp-content/uploads/2016/06/small_1420.png" />
-							<p>Sentiment Analysis of data using our robust machine learning algorithm</p>
-						</div>
-						<div className="col-md-4">
-							<img className="img-responsive" src="http://bav0.com/wp-content/uploads/sites/11/2014/06/pie-chart.png" height="250"/>
-							<p>Portfolio optimization and financial literacy to help any beginner investor make smarter choices</p>
-						</div>
-					</div>
-					<div className="row">
-						
-					</div>
-					<div className="row">
-						
-					</div>
+					</Jumbotron>
 				</div>
 			</DocumentTitle>
 		);
