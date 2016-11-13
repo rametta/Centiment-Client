648a9bd2e384bf7b2742b11da550a0fdf983fe64
Jason <rametta@outlook.com>
2016-11-13 00:46:04 -0500

    added homepage

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -7,8 +7,31 @@ export default class HomePage extends Component {
 	render() {
 		return (
 			<DocumentTitle title="Home">
-				<div>
-					<Logo />
+				<div className="container">
+					<div className="row text-center">
+						<h1>Do you want to make cents?</h1>
+					</div> 
+					<div className="row">
+						<div className="col-md-4">
+							<img className="img-responsive" src="https://coronadotimes.com/wp-content/uploads/2016/06/ascending-graph.png" height="250" />
+							<p>Performance metrics tracking on  top news 
+and social media content</p>
+						</div>
+						<div className="col-md-4">
+							<img className="img-responsive" height="150" src="https://www.deepcoredata.com/wp-content/uploads/2016/06/small_1420.png" />
+							<p>Sentiment Analysis of data using our robust machine learning algorithm</p>
+						</div>
+						<div className="col-md-4">
+							<img className="img-responsive" src="http://bav0.com/wp-content/uploads/sites/11/2014/06/pie-chart.png" height="250"/>
+							<p>Portfolio optimization and financial literacy to help any beginner investor make smarter choices</p>
+						</div>
+					</div>
+					<div className="row">
+						
+					</div>
+					<div className="row">
+						
+					</div>
 				</div>
 			</DocumentTitle>
 		);
